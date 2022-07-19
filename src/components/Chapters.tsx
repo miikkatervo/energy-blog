3bf5fe105b30b1b63f8085936872363b453519c3
Miikka Tervo <miikka.tervo@gmail.com>
2022-07-19 17:29:33 +0300

    add head tag

--- src/components/Chapters.tsx
+++ src/components/Chapters.tsx
@@ -21,6 +21,7 @@ const Chapters: React.FC<ChaptersProps> = ({ data }) => {
 
     return parsedText;
   };
+  let paragraph_id = 0; //unigue id for each paragraph
 
   return (
     <Box>
@@ -33,8 +34,9 @@ const Chapters: React.FC<ChaptersProps> = ({ data }) => {
                 <Box key={c.chapter_id} mt={30}>
                   <Heading size="md">{c.heading}</Heading>
                   {parseText(c.text).map((p) => {
+                    paragraph_id += 1;
                     return (
-                      <Box mt={5} mb={5}>
+                      <Box key={paragraph_id} mt={5} mb={5}>
                         <p>{p}</p>
                       </Box>
                     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import { NextPage } from "next";
 import { Layout } from "../components/Layout";
 import { data } from "../../data";
 import Chapters from "../components/Chapters";
+import Head from "next/head";
 
 const Index: NextPage = () => {
   return (
     <Layout data={data}>
+      <Head>
+        <title>Our Shift to Clean Energy</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Chapters data={data} />
     </Layout>
   );
