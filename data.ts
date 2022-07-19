7a052bed6bbb23eb65d7b15dbf284cae1f2ea7e2
Miikka Tervo <miikka.tervo@gmail.com>
2022-07-19 15:27:45 +0300

    initial version of site complete

---
+++ data.ts
@@ -0,0 +1,108 @@
+export const data = [
+  {
+    topic_id: 1,
+    title: "1. How we got here",
+    chapters: [
+      {
+        chapter_id: 1,
+        heading: '"Can somebody else do it?”',
+        text: `Let's get straight to the point. Humans don’t like to do hard, manual work, so we had animals, slaves and eventually machines do the work for us.
+
+        With the creation of the steam engine, humans could grasp the power of hundreds of horses in one compact device. The steam engine utilizes flammable substances such as coal to boil water. The steam from the boiling water creates pressure inside a cylinder and moves a piston and creates kinetic energy. This technology was adapted to many fields such as transportation, and thus the industrial revolution began.
+        
+        `,
+      },
+      {
+        chapter_id: 2,
+        heading: "The Greenhouse Effect",
+        text: `Ever since the creation of the steam engine, humans have been burning coal and other FF (fossil fuels) for power, and things were going well. During the 1950s though, the first concerns of climate change were rising. The greenhouse -effect was for a long time under debate if it was caused by humans or a natural phenomenon (the oil companies lobbying for the latter), eventually being broadly recognised as mostly being the aftermath of humans emitting GGs (greenhouse gasses) into the atmosphere. It is natural that there are GG such as carbon dioxide in the atmosphere, we have just largely increased the amount.
+
+        GGs are emitted into the atmosphere mainly by burning FFs such as coal, oil and natural gas. Other sources of GG exist, such as cow farts, but we won’t be going over them here. The term greenhouse effect -spurs from the fact that like a greenhouse, the heat from the sun is trapped in the earth due to the GG filled atmosphere. The amount of GG in the atmosphere has risen due to the amount of modern machines burning FF for power. The greenhouse effect has led to a steady increase in the earth's average temperature.
+        
+        Ever since the effects of climate change were realized, climate activists have been pursuing the adaptation of “clean” forms of energy, which don’t emit GG into the atmosphere.
+        
+        `,
+      },
+      {
+        chapter_id: 3,
+        heading: "EU Climate Law",
+        text: `In 2021 the EU has set climate targets to fight against climate change. The main goal is for Europe to be climate-neutral by 2050. An intermittent goal is to reduce net GG emissions by 55% compared to 1990 by 2030. To fight against global warming, we must firstly stop emitting GG into the atmosphere more than the earth can bind in trees and other carbon based structures, and eventually start removing GG particles from the atmosphere.
+
+        So climate-neutrality is only half the battle. To actually stop the earth from warming, we must also get the GG particles out of the atmosphere via CCS (Carbon Capture and Storage) or other means.
+        
+        Currently we are not on course to achieve climate-neutrality by 2050. This hard task cannot be left up to altruism and “doing the right thing”. We cannot expect all decision makers to have humanities best interests first in mind. I am not saying we are a greedy species and there’s nothing we can do to be our own downfall, but we cannot be too naive as well. The key is to innovate climate-friendly alternatives to existing GG emitting solutions, which are on par if not cheaper than what we have been doing.
+        `,
+      },
+    ],
+  },
+  {
+    topic_id: 2,
+    title: "2. How do we fix the problem?",
+    chapters: [
+      {
+        chapter_id: 1,
+        heading: "What should we do?",
+        text: `The popular theme regarding energy production is moving from fossil fuels to RES (renewable energy sources). Fossil fuels are resources found in the earth’s crust, such as coal, oil and natural gas. By burning these substances we can make a combustion engine equipped car move, but also emit carbon dioxide into the atmosphere as a byproduct.
+
+        The process of burning a resource to produce energy is not always seen as a harmful process. For example in some bioplants the excess bark and other material from trees which cannot be used for construction are burned and used as energy. This process still emits carbon dioxide, but the process is seen as climate-neutral, because trees also suck carbon from the air.
+        
+        Climate-neutrality means that we don’t add GG such as carbon dioxide to the atmosphere. This can be achieved by two ways:
+        1. No more burning fossil fuels
+        2. We burn fossil fuels but don’t let the GG into the atmosphere
+        
+        The first option is of course the better way to go about it, due to fossil fuels being a nonrenewable resource, which we will run out of eventually. On the other hand, we won’t be powering every car or plane with electricity overnight, thus innovation in harnessing carbon dioxide right after it is formed or pulling it from the air would really help during the transition period and eventually when we are climate-neutral and want to begin removing GG from the atmosphere.
+        
+        But for now let’s concentrate on not burning fossil fuels anymore. 
+        
+        OK, let’s say that all the countries in the world decide today that as of tomorrow, the usage of fossil fuels and emitting GG is strictly banned. Hooray right? Well, as you can also figure out, no, It would be a catastrophe. 
+        `,
+      },
+      {
+        chapter_id: 2,
+        heading: "The switch",
+        text: `To achieve climate-neutrality we must switch fossil fuels to RES (renewable energy sources). As the name suggests renewables are not a finite source, thus they are mostly sustainable, but above all they do not add GG to the atmosphere. Popular RES are solar, hydro,  wind and wave power and geothermal heat. 
+
+        Most RES cannot be used as a power source by itself. Of course there are some instances, such as sailing ships moving with wind power, but the world has accustomed to the instant and reliable power that fossil fuels can provide, thus electrification has become a crucial part of the equation.
+        
+        Electrification means turning appliances such as cars from running on fossil fuels to running on electricity. Electricity in itself is not something that can be mined from the earth, but it is an adapter of energy from wind to moving a car.
+        Electricity can be produced in different ways, mainly with turbines, but also with solar panels. Turbines convert kinetic energy into electricity, by a force spinning a coil, which produces a current of electricity. 
+        
+        The force for the spinning can be produced in different ways. Wind turbines are powered by moving air (the sun), water turbines by moving water (gravity) etc. Fossil fuels are also used to power turbines, mostly by steaming water which creates pressure and rotates the turbine. Nuclear power generates electricity in a similar fashion, but it does not emit GG. On the other hand, nuclear fusion is at the moment unsustainable, for the amount of uranium used in the process is finite on earth.
+        `,
+      },
+      {
+        chapter_id: 3,
+        heading: "Turbines",
+        text: `OK so let’s just change the fossil fuel turbines to RES turbines and turn everything electric?
+
+        Well this is a plan, but it is not so simple. At the moment, our electrical infrastructure is based on hourly adjusting production to the usage. By usage I mean when people use electric powered devices. This means when the need for electricity spikes, usually in the evening when people watch tv and heat up their sauna, the production has to spike up as well.
+        
+        This means we need control and reliability in our energy production, what FF turbines provide, and RES turbines unfortunately do not. As you can imagine, If the need for electricity increases, it is a lot easier to just add more fuel to the fire than make the wind blow harder or the sun to shine brighter. This is not the case with all RES turbines, since the output from hydropower turbines is pretty controllable, by allowing more water to pass through if needed. This is the same with nuclear power. Unfortunately there is not enough hydropower resources and nuclear energy to fill in the role of FF turbines. The use of hydropower is highly regulated as well.
+        
+        To solve the power of reliability in using wind and solar power, innovation is needed. An option would be to store power when production is more than usage, then use the stored power when production is less than usage.
+        
+        Storing energy is still a problem. When it comes to storing electricity you might think of batteries, but they are still not scalable outside of powering our car, but making huge batteries is still under development.
+        
+        One thing to touch is that energy doesn’t have to be morphed into electricity. One hot topic at the moment is hydropower (this is related to the atom hydrogen, not water). RES turbines can produce hydrogen, which can be used as a fuel as is, or used to store energy, and then be converted into electricity when needed. This is still very much in development. 
+        
+        But in any case if we need to switch from FF turbines to RES, we need more of it. As we speak, more wind and hydro turbines and solar panels are being constructed, and a new nuclear reactor is being tested in Olkiluoto.
+        
+        Constructing new turbines and plants is not as simple as you would think. There is a ton of regulation and restrictions on where you can build and how much. Because RES utilizes the natural resources of the earth, it also affects the natural wildlife. Countries such as Germany have even banned the production of nuclear power, due to health and security reasons.
+        
+        Wind turbines are still seen as ugly and loud, so they have to be constructed away from civilization, but also in areas where the wind constantly blows, usually in the coasts. The amount of useful land is limited, so new off-shore wind turbines have been in development. By constructing the wind turbines off-shore, we can have more and larger turbines, which can be grouped up in farms. The trade-off is that the costs for construction and maintenance are higher compared to on-shore. Still the amount of off-shore wind turbines is skyrocketing, and coastal countries such as Denmark and Great Britain are investing heavily on it to solve their energy needs. Tide and wave energy is also being researched as we speak.
+        
+        When you try to research RES opportunities, it gets muddy fast. Things are never as simple as it seems. Even though RES production doesn’t emit GG, it does so indirectly. The amount of GG emitted is usually pretty high during construction and maintenance of new turbines and facilities, and during the manufacturing of the parts for the new installation. As said before, RES also affects the natural wildlife and usually requires a lot of space, which might end up destroying some plants and animal’s homes. It’s great that an electric car doesn’t emit GG while driving, but the amount of GG emitted during the manufacturing of the car and battery are usually really high. 
+        
+        Change is also not cheap. 
+        
+        This might seem that whatever we do, it always has some downside. This is true to almost anything. And it is great that ideas and decisions are challenged, this way we can learn and be better. But in the end, change HAS to come.
+        `,
+      },
+    ],
+  },
+  {
+    topic_id: 3,
+    title: "3. New Innovation",
+    chapters: [],
+  },
+];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import "@fontsource/merriweather/300.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/roboto/500.css";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme";
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
