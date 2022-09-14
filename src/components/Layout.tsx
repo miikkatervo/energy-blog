57fbcffd69574b0f372db681606ae1fb4d1dc4c1
Miikka Tervo <miikka.tervo@gmail.com>
2022-09-14 12:09:52 +0300

    add mobile friendliness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { NavBar } from "./NavBar";
 import SideBar from "./SideBar";
 import { WrapperVariant, Wrapper } from "./Wrapper";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 interface LayoutProps {
   variant?: WrapperVariant;
@@ -11,15 +12,23 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children, variant, data }) => {
+  const isDesktop = useMediaQuery("(min-width: 800px)");
+
   return (
     <>
-      <NavBar />
-      <Flex>
-        <SideBar data={data} />
-        <Box ml={110}>
+      <NavBar isDesktop={isDesktop} />
+      {isDesktop ? (
+        <Flex>
+          <SideBar data={data} />
+          <Box ml={"5em"} mr={"5em"}>
+            <Wrapper variant={variant}>{children}</Wrapper>
+          </Box>
+        </Flex>
+      ) : (
+        <Box ml={"3em"} mr={"3em"}>
           <Wrapper variant={variant}>{children}</Wrapper>
         </Box>
-      </Flex>
+      )}
     </>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,39 @@
 import { Flex, Heading, Link, Text, Box } from "@chakra-ui/react";
 import NextLink from "next/link";
 
-interface NavBarProps {}
+interface NavBarProps {
+  isDesktop: boolean;
+}
 
-export const NavBar: React.FC<NavBarProps> = ({}) => {
+export const NavBar: React.FC<NavBarProps> = ({ isDesktop }) => {
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="#107dac" p={4} height={20}>
       <Flex flex={1} m="auto" align="center" maxW={1000}>
         <NextLink href="/">
           <Link>
-            <Heading color="white">
-              Our Shift to Clean Energy (In a Nutshell)
-            </Heading>
+            {isDesktop ? (
+              <Heading color="white">
+                Our Shift to Clean Energy (In a Nutshell)
+              </Heading>
+            ) : (
+              <Heading as="h4" fontSize={16} color="white">
+                Our Shift to Clean Energy (In a Nutshell)
+              </Heading>
+            )}
           </Link>
         </NextLink>
         <Box height="100%" position="relative" ml={3}>
-          <Box mt={4}>
-            <Text color="white">By Miikka Tervo</Text>
-          </Box>
+          {isDesktop ? (
+            <Box mt={4}>
+              <Text color="white">By Miikka Tervo</Text>
+            </Box>
+          ) : (
+            <Box mt={2}>
+              <Text fontSize={12} color="white">
+                By Miikka Tervo
+              </Text>
+            </Box>
+          )}
         </Box>
       </Flex>
     </Flex>
